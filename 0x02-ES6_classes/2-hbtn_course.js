17d606a1df18d683244fa05de9a784db2f0a3992
Joshua <joshuaosaro20@gmail.com>
2023-04-20 14:50:57 +0000

    complete task 2

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,46 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this.students = students;
+  }
+
+  //   getter and setter for name attribute
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a String');
+    }
+    this._name = value;
+  }
+
+  //   getter and setter for length attribute
+  get length() {
+    return this._length;
+  }
+
+  set length(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Lenth must be a Number');
+    }
+    this._length = value;
+  }
+
+  //   getter and setter for students attribute
+  get students() {
+    return this._name;
+  }
+
+  set students(value) {
+    if (!(value instanceof Array)) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    if (!value.every((student) => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = value;
+  }
+}
